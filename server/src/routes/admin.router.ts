36dd5d32b051f201b44683c4d7a027cb55a4bd9c
Nishan Dhakal <nissandhakal1@gmail.com>
2026-01-15 18:20:18 +0545

    route fix

--- server/src/routes/admin.router.ts
+++ server/src/routes/admin.router.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import * as adminController from '../controllers/admin.controller.ts';
+import { protect } from '../middleware/auth.middleware.ts';
 
 const router = express.Router();
 //order management
-router.get('/orders/active', adminController.getActiveOrders);
-router.get('/orders/history', adminController.getOrderHistory);
-router.patch('/orders/:orderId/serve', adminController.serveOrder);
-router.get('/orders/:orderId/bill', adminController.getBill);
+router.get('/orders/active', protect, adminController.getActiveOrders);
+router.get('/orders/history', protect, adminController.getOrderHistory);
+router.patch('/orders/:orderId/serve', protect, adminController.serveOrder);
+router.get('/orders/:orderId/bill', protect, adminController.getBill);
 
 // Item management
-router.post('/orders/:orderId/items', adminController.addItemsToOrder);
-router.patch('/orders/:orderId/items/:menuItemId/reduce', adminController.reduceOrderItem);
-router.delete('/orders/:orderId/items/:menuItemId', adminController.cancelOrderItem);
+router.post('/orders/:orderId/items', protect, adminController.addItemsToOrder);
+router.patch('/orders/:orderId/items/:menuItemId/reduce', protect, adminController.reduceOrderItem);
+router.delete('/orders/:orderId/items/:menuItemId', protect, adminController.cancelOrderItem);
 
 export default router;
